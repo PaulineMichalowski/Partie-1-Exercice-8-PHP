45b0cc75dab22731eec8da63c3aa593d49ed59db
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-12 11:16:24 +0100

    Création de l'exercice 8

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$premiere_operation = 3 + 4;
+$seconde_operation = 5 * 20;
+$troisieme_operation = 45 / 5;
+echo $premiere_operation . ' '. $seconde_operation . ' ' . $troisieme_operation;
+ ?>
